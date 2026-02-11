07c6672c9be28b927276293065d27af3b5ab8ee6
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-11 23:31:00 +0800

    PUT products update product handler and endpoint

--- src/Services/Catalog/Catalog.API/Products/Commands/UpdateProduct/UpdateProductCommandEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/Commands/UpdateProduct/UpdateProductCommandEndpoint.cs
@@ -1,5 +1,29 @@
 ﻿namespace Catalog.API.Products.Commands.UpdateProduct;
 
-public class UpdateProductCommandEndpoint
+public record UpdateProductCommandRequest(
+    Guid Id,
+    string Name,
+    List<string> Categories,
+    string Description,
+    string ImageUrl,
+    decimal Price);
+public record UpdateProductCommandResponse(bool IsSuccess);
+public class UpdateProductCommandEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductCommandRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateProductCommandRequest>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<UpdateProductCommandResponse>();
+            return Results.Ok(response);
+        })
+            .WithTags("Products")
+            .WithName("UpdateProduct")
+            .Produces<UpdateProductCommandResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Updates an existing product")
+            .WithDescription("Updates an existing product with the provided id");
+    }
 }
