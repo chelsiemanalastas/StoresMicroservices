47eed218fb4c6f69e9a040ddf0bed8aefce038a2
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-11 23:29:55 +0800

    Rename records to specify action in update command

--- src/Services/Catalog/Catalog.API/Products/Commands/CreateProduct/CreateProductCommandEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/Commands/CreateProduct/CreateProductCommandEndpoint.cs
@@ -1,28 +1,28 @@
 ﻿namespace Catalog.API.Products.Commands.CreateProduct;
 
-public record CreateProductRequest(
+public record CreateProductCommandRequest(
     string Name,
     List<string> Categories,
     string Description,
     string ImageUrl,
     decimal Price);
 
-public record CreateProductResponse(Guid Id);
+public record CreateProductCommandResponse(Guid Id);
 
 public class CreateProductCommandEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        app.MapPost("/products", async (CreateProductCommandRequest request, ISender sender) =>
         {
             var command = request.Adapt<CreateProductCommand>();
             var result = await sender.Send(command);
-            var response = result.Adapt<CreateProductResponse>();
+            var response = result.Adapt<CreateProductCommandResponse>();
             return Results.Created($"/products/{response.Id}", response);
         })
             .WithTags("Products")
             .WithName("CreateProduct")
-            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .Produces<CreateProductCommandResponse>(StatusCodes.Status201Created)
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .WithSummary("Creates a new product")
             .WithDescription("Creates a new product with the provided details.");
