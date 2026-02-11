73d743574dace167e049f8efd47a39b2be66cdb6
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-11 23:06:32 +0800

    GET products by category handler and endpoint

--- src/Services/Catalog/Catalog.API/Products/Queries/GetProductsByCategory/GetProductByCategoryQueryEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/Queries/GetProductsByCategory/GetProductByCategoryQueryEndpoint.cs
@@ -1,5 +1,22 @@
 ﻿namespace Catalog.API.Products.Queries.GetProductsByCategory;
 
-public class GetProductByCategoryQueryEndpoint
+public record GetProductByCategoryQueryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryQueryEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("products/category/{category}", async (string category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsByCategoryQuery(category));
+            var response = result.Adapt<GetProductByCategoryQueryResponse>();
+            return Results.Ok(response);
+        })
+            .WithTags("Products")
+            .WithName("GetProductsByCategory")
+            .Produces<GetProductsByCategoryQuery>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Gets all products by category")
+            .WithDescription("Gets the list of products filtered by provided category");
+    }
 }
