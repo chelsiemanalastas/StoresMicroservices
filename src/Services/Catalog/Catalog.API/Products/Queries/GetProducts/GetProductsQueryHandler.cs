3712267349288e2748702e2d1d0074991fd87b93
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-11 18:23:44 +0800

    GET products query endpoint and handler

--- src/Services/Catalog/Catalog.API/Products/Queries/GetProducts/GetProductsQueryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/Queries/GetProducts/GetProductsQueryHandler.cs
@@ -1,6 +1,19 @@
-﻿namespace Catalog.API.Products.Queries.GetProducts;
+﻿using BuildingBlocks.CQRS;
 
-public class GetProductsQueryHandler
+namespace Catalog.API.Products.Queries.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+
+public class GetProductsQueryHandler(
+        IDocumentSession documentSession,
+        ILogger<GetProductsQueryHandler> logger
+    ) : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
-
+    public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Handling GetProductsQuery");
+        var products = await documentSession.Query<Product>().ToListAsync(cancellationToken);
+        return new GetProductsResult(products);
+    }
 }
