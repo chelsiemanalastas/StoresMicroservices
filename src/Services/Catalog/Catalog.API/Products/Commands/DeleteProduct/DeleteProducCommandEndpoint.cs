0340a5c2df220feef12ad1d75f5db8594281123f
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-12 15:05:14 +0800

    DELETE products by id handler and endpoint

--- src/Services/Catalog/Catalog.API/Products/Commands/DeleteProduct/DeleteProducCommandEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/Commands/DeleteProduct/DeleteProducCommandEndpoint.cs
@@ -1,5 +1,24 @@
 ﻿namespace Catalog.API.Products.Commands.DeleteProduct;
 
-public class DeleteProducCommandEndpoint
+public record DeleteProductCommandRequest(Guid Id);
+public record DeleteProductCommandResponse(bool IsSuccess);
+
+public class DeleteProducCommandEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            var command = new DeleteProductByIdCommand(id);
+            var result = await sender.Send(command);
+            var response = result.Adapt<DeleteProductCommandResponse>();
+            return Results.Ok(response);
+        })
+            .WithTags("Products")
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductCommandResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithSummary("Deletes an existing product")
+            .WithDescription("Permanently deletes an existing product with the provided id");
+    }
 }
