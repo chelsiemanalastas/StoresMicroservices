53e063eb950360fafd5ae741b76ba40117f4c7fb
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-13 16:45:52 +0800

    Add product id validation rule and delete product command

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductCommandValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Products.DeleteProduct;
+
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductByIdCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(x => x.Id).ValidId();
+    }
+}
