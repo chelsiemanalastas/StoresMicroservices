b7de13b205d0f5c68505a14dbdd8ee19c8d5e13d
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-13 15:09:02 +0800

    Require id in update product command

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandValidator.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandValidator.cs
@@ -4,6 +4,9 @@ public class UpdateProductCommandValidator : AbstractValidator<UpdateProductComm
 {
     public UpdateProductCommandValidator()
     {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Product Id to update is required.");
         RuleFor(x => x.Name).ValidName();
         RuleFor(x => x.Categories).ValidCategories();
         RuleFor(x => x.Description).ValidDescription();
