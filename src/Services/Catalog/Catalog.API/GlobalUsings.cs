407b4920ecff41bd82f133271ee1313a5d76eb2c
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-13 15:06:05 +0800

    Add shared product validation rules

--- src/Services/Catalog/Catalog.API/GlobalUsings.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -1,9 +1,11 @@
 ﻿global using BuildingBlocks.CQRS;
 global using Carter;
 global using Catalog.API.Models;
+global using Catalog.API.Products.Shared;
 global using Catalog.Domain.Exceptions;
 global using FluentValidation;
 global using Mapster;
 global using Marten;
 global using MediatR;
+
 
