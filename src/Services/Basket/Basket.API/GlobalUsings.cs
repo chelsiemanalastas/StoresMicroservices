203327cef9b3c4712d4ac3bed8ddf7fe5e7a2e39
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-15 21:46:39 +0800

    Register Carter and MediatR in program file including behaviors

--- src/Services/Basket/Basket.API/GlobalUsings.cs
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -1,8 +1,11 @@
 ﻿global using Basket.API.Models;
+global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.CQRS;
 global using Carter;
 global using FluentValidation;
 global using Mapster;
 global using MediatR;
+
+
 
 
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,8 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var services = builder.Services;
+var assembly = typeof(Program).Assembly;
+
+services.AddCarter();
+services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(assembly);
+    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
