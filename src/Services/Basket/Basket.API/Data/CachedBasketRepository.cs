7db6574ddbdccb25d3ac2af294ed3bcf9a377dfc
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-16 21:41:46 +0800

    Add CachedBasketRepository

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Data;
+
+public class CachedBasketRepository(IBasketRepository basketRepository) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasketAsync(string username, CancellationToken cancellationToken = default)
+    {
+        return await basketRepository.GetBasketAsync(username, cancellationToken);
+    }
+
+    public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        return await basketRepository.StoreBasketAsync(basket, cancellationToken);
+    }
+    public async Task<bool> DeleteBasketAsync(string username, CancellationToken cancellationToken = default)
+    {
+        return await basketRepository.DeleteBasketAsync(username, cancellationToken);
+    }
+}
