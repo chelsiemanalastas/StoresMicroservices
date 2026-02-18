48312b35eb90577e3c5eb1873497f1fd88cbfa7e
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-18 18:22:30 +0800

    Implement DiscountProtoService in StoreBasket handler and register grpc in program file

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,16 +1,28 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Discount.Grpc;
+
+namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
 public record StoreBasketResult(string Username);
 public class StoreBasketCommandHandler(
-        IBasketRepository basketRepository
+        IBasketRepository basketRepository,
+        DiscountProtoService.DiscountProtoServiceClient discountProto
     ) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        ShoppingCart cart = command.Cart;
-        await basketRepository.StoreBasketAsync(cart, cancellationToken);
+        await DeductDiscount(command.Cart, cancellationToken);
+        await basketRepository.StoreBasketAsync(command.Cart, cancellationToken);
 
         return new StoreBasketResult(command.Cart.Username);
+    }
+
+    public async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var discount = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= discount.Amount;
+        }
     }
 }
