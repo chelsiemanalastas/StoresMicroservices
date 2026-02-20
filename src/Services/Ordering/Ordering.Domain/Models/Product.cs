ae76c57a8e54cbde3e99613d0d59f585d7376b90
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-20 19:49:21 +0800

    Develop rich-domain model for order entities in Order.Domain

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -4,4 +4,17 @@ public class Product : Entity<ProductId>
 {
     public string Name { get; private set; } = default!;
     public string Price { get; private set; } = default!;
+
+    public static Product Create(ProductId id, string name, string price)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentException.ThrowIfNullOrWhiteSpace(price);
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Price = price
+        };
+        return product;
+    }
 }
