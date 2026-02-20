5404386a8147930919c81797caaa6ca3e2f354ff
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-20 22:25:28 +0800

    Add entities configuration in Ordering.Infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/ProductConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class ProductConfig : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasConversion(
+                productId => productId.Value,
+                dbId => ProductId.Of(dbId)
+            );
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(250)
+            .IsRequired();
+    }
+}
