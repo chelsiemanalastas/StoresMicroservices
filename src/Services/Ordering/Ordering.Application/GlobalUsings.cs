b01dbf9f87be10ea629283d5551c11d0b26755b1
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-22 21:07:15 +0800

    Develop update order command and handler

--- src/Services/Ordering/Ordering.Application/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsings.cs
@@ -1,2 +1,8 @@
-﻿global using Ordering.Domain.Models;
+﻿global using BuildingBlocks.CQRS;
+global using FluentValidation;
+global using Ordering.Application.Data;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Exceptions;
+global using Ordering.Application.Orders.Shared;
+global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
