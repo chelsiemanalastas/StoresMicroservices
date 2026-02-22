2ef958b7c0e204278355adc37bbe63aa60c8b30f
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-22 17:49:08 +0800

    Introduct IApplicationDbContext abstraction and register implementation in infrastructure layer ordering microservices

--- src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
@@ -1,5 +1,14 @@
-﻿namespace Ordering.Application.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+
+namespace Ordering.Application.Data;
 
 public interface IApplicationDbContext
 {
+    DbSet<Customer> Customers { get; }
+    DbSet<Product> Products { get; }
+    DbSet<Order> Orders { get; }
+    DbSet<OrderItem> OrderItems { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
