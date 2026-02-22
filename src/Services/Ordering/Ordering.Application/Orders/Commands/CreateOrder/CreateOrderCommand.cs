ceace6300e3e1cf0a999dea9e5a585263aa17cf7
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-22 17:37:07 +0800

    Add CreateOrder command and validation

--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -1,5 +1,19 @@
-﻿namespace Ordering.Application.Orders.Commands.CreateOrder;
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+using Ordering.Application.Orders.Shared;
 
-public class CreateOrderCommand
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
 {
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x => x.Order.OrderName).ValidName();
+        RuleFor(x => x.Order.CustomerId).ValidId("Customer");
+        RuleFor(x => x.Order.OrderItems).ValidOrderItems();
+    }
 }
--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,5 +1,11 @@
-﻿namespace Ordering.Application.Orders.Commands.CreateOrder;
+﻿using BuildingBlocks.CQRS;
 
-public class CreateOrderCommandHandler
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderCommandHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
 {
+    public async Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
