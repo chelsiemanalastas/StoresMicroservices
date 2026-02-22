bb170b2d938df8f33807cfab0b05cf475eab11c0
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-22 14:54:48 +0800

    Setup folders and files for Ordering.Application layer in Ordering microservices

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Application.Exceptions;
+
+internal class OrderNotFoundException
+{
+}
---
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Application.Extensions;
+
+internal class OrderExtensions
+{
+}
