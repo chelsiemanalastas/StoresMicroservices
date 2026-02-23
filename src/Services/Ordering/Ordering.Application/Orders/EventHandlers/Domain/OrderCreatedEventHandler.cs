1f7887b465d420eeee3824d3866a6d2a39f2e226
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-23 13:28:29 +0800

    Initial implementation of order created and order updated event handler

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,5 +1,13 @@
 ﻿namespace Ordering.Application.Orders.EventHandlers.Domain;
 
-public class OrderCreatedEventHandler
+public class OrderCreatedEventHandler(
+        ILogger<OrderCreatedEventHandler> logger
+    ) : INotificationHandler<OrderCreatedEvent>
 {
+    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        // TODO: implementation
+        logger.LogInformation("Handling OrderCreatedEvent for OrderId: {OrderId}, event name: {DomainEvent}", notification.order.Id, notification.GetType().Name);
+        return Task.CompletedTask;
+    }
 }
