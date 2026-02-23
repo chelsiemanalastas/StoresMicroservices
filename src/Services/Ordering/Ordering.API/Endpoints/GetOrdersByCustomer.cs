237dd557a1a97fa696a78d95c85f942ea8972738
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-23 21:51:43 +0800

    Develop get orders by customer endpoint in ordering microservice

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -1,5 +1,24 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
 
-public class GetOrdersByCustomer
+namespace Ordering.API.Endpoints;
+
+public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+public class GetOrdersByCustomer : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByCustomerQuery(customerId));
+            var response = result.Adapt<GetOrdersByCustomerResponse>();
+            return Results.Ok(response);
+        })
+        .WithTags("Order")
+        .WithName("GetOrdersByCustomer")
+        .Produces(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Gets all orders by customer")
+        .WithDescription("Gets all orders of a customer");
+    }
 }
