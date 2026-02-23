4969bcf076d48f844d55cc32b706c6e71f8d5c3c
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-23 21:55:30 +0800

    Develop get orders endpoint with pagination in ordering microservice

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,5 +1,25 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
 
-public class GetOrders
+namespace Ordering.API.Endpoints;
+
+public record GetOrdersReponse(PaginatedResult<OrderDto> Orders);
+public class GetOrders : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] PaginationRequest request,ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersQuery(request));
+            var response = result.Adapt<GetOrdersReponse>();
+            return Results.Ok(response);
+        })
+        .WithTags("Order")
+        .WithName("GetOrders")
+        .Produces(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Gets all orders")
+        .WithDescription("Gets all orders in the system");
+    }
 }
