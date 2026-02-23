01cc0c0e8c0aebcc2d60519d2e61d5e1633423dd
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-23 18:17:06 +0800

    Create order extension to map Order to OrderDto

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderByName/GetOrderByNameQueryHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrderByName/GetOrderByNameQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Ordering.Application.Orders.Queries.GetOrderByName;
+﻿using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrderByName;
 
 public class GetOrderByNameQueryHandler(
         IApplicationDbContext dbContext
@@ -14,64 +16,7 @@ public class GetOrderByNameQueryHandler(
             .OrderBy(o => o.OrderName)
             .ToListAsync(cancellationToken);
 
-        var orderDtos = ProjectToOrderDto(orders);
-
-        return new GetOrderByNameResult(orderDtos);
+        return new GetOrderByNameResult(orders.ToOrderDtoList());
     }
-
-    private List<OrderDto> ProjectToOrderDto(List<Order> orders)
-    {
-        List<OrderDto> result = new();
-        foreach (var order in orders)
-        {
-            var orderDto = new OrderDto(
-                Id: order.Id.Value,
-                CustomerId: order.CustomerId.Value,
-                OrderName: order.OrderName.Value,
-                ShippingAddress: new AddressDto
-                (
-                    order.ShippingAddress.FirstName,
-                    order.ShippingAddress.LastName,
-                    order.ShippingAddress.EmailAddress,
-                    order.ShippingAddress.AddressLine,
-                    order.ShippingAddress.Country,
-                    order.ShippingAddress.State,
-                    order.ShippingAddress.ZipCode
-                ),
-                BillingAddress: new AddressDto
-                (
-                    order.BillingAddress.FirstName,
-                    order.BillingAddress.LastName,
-                    order.BillingAddress.EmailAddress,
-                    order.BillingAddress.AddressLine,
-                    order.BillingAddress.Country,
-                    order.BillingAddress.State,
-                    order.BillingAddress.ZipCode
-                ),
-                Payment: new PaymentDto
-                (
-                    order.Payment.CardName,
-                    order.Payment.CardNumber,
-                    order.Payment.Expiration,
-                    order.Payment.CVV,
-                    order.Payment.PaymentMethod
-                ),
-                Status: order.Status,
-                OrderItems: order.OrderItems
-                .Select(oi => new OrderItemDto
-                (
-                    oi.OrderId.Value,
-                    oi.ProductId.Value,
-                    oi.Quantity,
-                    oi.Price
-                )).ToList()
-            );
-
-            result.Add(orderDto);
-        }
-
-        return result;
-    }
-
 
 }
