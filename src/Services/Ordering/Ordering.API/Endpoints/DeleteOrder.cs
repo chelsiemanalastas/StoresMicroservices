1146ad52a5d0bbd21730d2431abdf1ff61a0e1ed
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-23 19:41:37 +0800

    Develop DeleteOrder endpoint in ordering microservice

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,5 +1,23 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
 
-public class DeleteOrder
+namespace Ordering.API.Endpoints;
+
+public record DeleteOrderResponse(bool IsSuccess);
+public class DeleteOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteOrderCommand(Id));
+            var response = result.Adapt<DeleteOrderResponse>(); 
+            return Results.Ok(response);
+        })
+        .WithTags("Order")
+        .WithName("DeleteOrder")
+        .Produces(StatusCodes.Status204NoContent)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Deletes an order")
+        .WithDescription("Deletes an order of a customer");
+    }
 }
