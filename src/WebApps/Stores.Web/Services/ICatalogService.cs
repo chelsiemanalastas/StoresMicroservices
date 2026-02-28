c1393ae5f37a9311b2e3a78047f615e7bdf259a2
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-28 21:50:23 +0800

    Add ICatalogService in stores.web

---
+++ src/WebApps/Stores.Web/Services/ICatalogService.cs
@@ -0,0 +1,10 @@
+﻿using Stores.Web.Models.Catalog;
+
+namespace Stores.Web.Services;
+
+public interface ICatalogService
+{
+    Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+    Task<GetProductByIdResponse> GetProductById(Guid Id);
+    Task<GetProductByCategoryResponse> GetProductByCategory(string category);
+}
