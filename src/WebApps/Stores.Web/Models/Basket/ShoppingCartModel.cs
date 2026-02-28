8ebaed99a497f6c05b78a7141f8e6e09b3548dfd
Chelsie Manalastas <manalastaschelsie@gmail.com>
2026-02-28 21:35:03 +0800

    Add shopping cart and shopping cart item models and wrapper classes for stores.web

---
+++ src/WebApps/Stores.Web/Models/Basket/ShoppingCartModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Stores.Web.Models.Basket;
+
+public class ShoppingCartModel
+{
+    public string Username { get; set; } = default!;
+    public List<ShoppingCartItemModel> Items { get; set; } = new();
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+}
+
+public class ShoppingCartItemModel
+{
+    public int Quantity { get; set; } = default!;
+    public string Color { get; set; } = default!;
+    public decimal Price { get; set; } = default!;
+    public Guid ProductId { get; set; } = default!;
+    public string ProductName { get; set; } = default!;
+}
+
+// wrapper classes
+public record GetBasketResponse(ShoppingCartModel Cart);
+public record GetBasketRequest(ShoppingCartModel Cart);
+public record StoreBasketResponse(string Username);
+public record DeleteBasketResponse(bool IsSuccess);
